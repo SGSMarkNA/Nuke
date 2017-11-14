cd71d64c0b66e5ac66b510aadd48223255f24e6e
Drew Loveridge <drew.loveridge@armstrong-white.com>
2017-11-14 15:12:57 -0500

    updates for nuke 10 folder creation

--- Nuke_Scripts/Callbacks/beforeFrameRender.py
+++ Nuke_Scripts/Callbacks/beforeFrameRender.py
@@ -5,4 +5,9 @@ except ImportError:
 	
 from Nuke_Scripts.SystemFns.files_and_folders import createWriteDir
 if not nuke == None:
-	nuke.addBeforeFrameRender(createWriteDir, (), {}, "Write")
+	# Nuke 10.5 now has a create_directories knob that will automatically create any non-existant directories.
+	# There is a knobDefault in app_initialization to turn on the knob's checkbox for any new Write nodes.
+	Major = nuke.NUKE_VERSION_MAJOR
+	Minor = nuke.NUKE_VERSION_MINOR
+	if Major <10:	
+		nuke.addBeforeFrameRender(createWriteDir, (), {}, "Write")
